2827167da40e05f2bf6e140bf8d1803b27897d9e
Ducaster <kooa3399@kakao.com>
2025-03-17 12:15:34 +0900

    aws

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -9,7 +9,9 @@ import Redis from 'ioredis';
         return new Redis({
           host: process.env.REDIS_HOST,
           port: Number(process.env.REDIS_PORT),
-          password: process.env.REDIS_PASSWORD,
+          // 비밀번호 제거
+          // password: process.env.REDIS_PASSWORD, // 비밀번호가 없으므로 주석 처리
+          tls: {}, // AWS MemoryDB는 TLS를 사용하므로 추가
         });
       },
     },
